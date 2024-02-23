c88da805fe55680ea6f3e0d3dafbbb204403f77b
Shasahnk Kumar Chaudhary <shashank.k.chaudhary@gmail.com>
2024-02-23 16:35:42 +0530

    Update ContactUs component styling

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -52,18 +52,18 @@ const ContactUs = () => {
   }, [formAlert]);
 
   return (
-    <div className='h-screen bg-[#1c1c36] bg-dancer1 bg-center bg-cover bg-blend-color-dodge sm:bg-gradient-to-r sm:to-[#131324] sm:from-[#120b2c] relative flex justify-around snap-start' id='contact'>
-        <div className='hidden sm:w-1/2 px-20 sm:flex sm:self-center'>
+    <div className='h-screen bg-[#1c1c36] bg-dancer1 bg-center bg-cover bg-blend-color-dodge sm:bg-gradient-to-r sm:to-[#131324] sm:from-[#120b2c] relative flex max-sm:flex-col-reverse justify-center gap-10 snap-start' id='contact'>
+        <div className='sm:w-1/2 flex items-center justify-center'>
             <div>
-                <p className='p-4 text-2xl  tracking-widest font-normal'>CONTACT INFORMATION</p>
-                <p className='flex items-center gap-5 p-4 text-xl font-light'><FaPhone color='#55549D'/> +202 555 0789</p>
-                <p className='flex items-center gap-5 p-4 text-xxl font-light'><AiOutlineMail color='#55549D' /> dance@email.com</p>
-                <p className='flex items-center gap-5 p-4 text-xxl font-light'><GrLocation color='#55549D' /> 218 Pegg Rd, Morris, New York(NY), 13808 </p>
+                <p className='p-2 text-2xl max-sm:text-base tracking-widest font-normal'>CONTACT INFORMATION</p>
+                <p className='flex items-center gap-5 p-2 max-sm:text-sm text-xl font-light'><FaPhone color='#55549D'/> +202 555 0789</p>
+                <p className='flex items-center gap-5 p-2 max-sm:text-sm text-xxl font-light'><AiOutlineMail color='#55549D' /> dance@email.com</p>
+                <p className='flex items-center gap-5 p-2 max-sm:text-sm text-xxl font-light'><GrLocation color='#55549D' /> 218 Pegg Rd, Morris, New York(NY), 13808 </p>
             </div>    
         </div>
-        <div className='max-w-max  sm:w-1/2 sm:px-24 self-center'>
+        <div className='sm:w-1/2 self-center flex justify-center flex-col items-center'>
               <p className='py-8 px-14 w-auto text-2xl tracking-widest font-normal'> CONTACT US</p>
-            <form className='flex flex-col  gap-10' onSubmit={querySubmitHandler}>
+            <form className='flex flex-col gap-5' onSubmit={querySubmitHandler}>
                 <input 
                 type="text" 
                 placeholder='Name' 
